50aa308da358a6944d07a87431ea0855c5938674
Daniel Friesel <derf@finalrewind.org>
2014-03-03 20:58:26 +0100

    version bump to 2.05

--- lib/Travel/Routing/DE/EFA/Route/Part.pm
+++ lib/Travel/Routing/DE/EFA/Route/Part.pm
@@ -6,7 +6,7 @@ use 5.010;
 
 use parent 'Class::Accessor';
 
-our $VERSION = '2.04';
+our $VERSION = '2.05';
 
 Travel::Routing::DE::EFA::Route::Part->mk_ro_accessors(
 	qw(arrival_platform arrival_stop
@@ -86,7 +86,7 @@ points, without interchanges
 
 =head1 VERSION
 
-version 2.04
+version 2.05
 
 =head1 DESCRIPTION
 
